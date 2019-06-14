bc8b579bf19e3fa9bbc5fe7a796d1cd4fa159f1b
Soliudeen999 <soliudeen999@gmail.com>
2019-06-14 15:52:25 +0100

    Parent Module handled completely and Student Module handled partially

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,31 @@ Route::post('/register/staff', 'Auth\RegisterController@createStaff');
 Route::post('/register/parent', 'Auth\RegisterController@createParent');
 Route::post('/register/student', 'Auth\RegisterController@createStudent');
 
+// Complete Registration form route
+Route::post('store/{id}', 'GuardianController@store')->name('saveProfile');
+
 Route::view('/home', 'home')->middleware('auth');
 Route::view('/admin', 'admin');
-Route::view('/parent', 'parent');
+Route::get('/parent', 'GuardianController@index');
+Route::get('/registration', 'GuardianController@registration')->name('createProfile');
+Route::post('/edit_profile/{id}', 'GuardianController@update')->name('editProfile');
+// view message and reply
+Route::get('view-message/{message_id}/{status}/{controller}', 'MessageController@view_message')->name('viewMessage');
+
+
+Route::get("message/{med}/{id}", "StudentController@message");
+// Route::redire("StudentController", "StudentController@message");
+// upload profile photo 
+Route::post('/upload/{id}', 'GuardianController@upload')->name('upload');
+
+// 
+// Student route
+// 
+Route::resource('/student', 'StudentController');
+// Registration 
+Route::get('/student_registration', 'StudentController@registration')->name('createStudentProfile');
+Route::post('/student_profile/{_id}', 'StudentController@update')->name('editStudentProfile');
+// Route::get('/registration', 'StudentController@registration')->name('createProfile');
+
+
+
